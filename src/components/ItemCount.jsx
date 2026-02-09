576538a1a0b9b7519bf80347139d1ce2c7325eee
Franco <francopicaroni20@gmail.com>
2026-02-09 15:53:18 -0300

    NavegaLasRutasPicaroni

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -39,23 +39,3 @@ const ItemCount = ({ stock = 0, initial = 1, onAdd }) => {
 
 export default ItemCount;
 
-//Aca van a ir los useeffect, que se ejecutan una vez(cUANDO SE MONTA UN COMPONENTE, O
-// S EA, CUANDO ALGO SE RECARGA Y SE HACE DE NUEVO POR EJEMPLO), por ejemplo cuando alguien compra
-//uN PRODUCTO O CUANDO SUMA O RESTA (O SEA, CUANDO SE ACTUALIZA UN PRODUCTO)
-
-//
-//Recuerdo armar los productos en un async
-//Averiguar como funicona prmise, reject y resolve
-//Si yo tengo algo que devuelve un array, como getProductos, al hacer el resolve(productos)
-//Voy a obtener la lista de productos
-//Then y catch, map (data.map )
-//Quiero poner un componenete de suma y resta en la zona del carritro
-
-//Configurar navegabilidad entre componentes usando router instalar reactrouter para usarlo, tambien
-//link y useParams para agarrar datos de la url y buscar productos en particular por ejemplo
-//Un ejemplo de un componente hijo mandando informacion a un padre, un contador que manda una cantidad al carrito
-//Siendo el padre el carrito, es como una division de tareas para no sobrecargar a por ejemplo a un contador 
-//Se le dice "delegar" responsabilidad
-//Armar un error.jsx para retornarlo en un componente
-//Tengo que crear item.jsx y el boton ver mas usarlo con {/* <Link to={'/item/'+prod.id} className='btn btn-dark'>Ver Más</Link> */}
-      //  <Link to={`/item/${prod.id}`} className='btn btn-dark'>Ver Más</Link>
